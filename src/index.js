edf5c9481766bbd0f105cb4a3b9ad085eaf98398
Milad Hosseinpour <m.hosseinpour@Rayan.Parsi>
2022-12-17 15:04:56 +0330

    revert to c3d52c3bfcde97ccea79479bd006975841d20040

--- src/index.js
+++ src/index.js
@@ -32,12 +32,6 @@ export default Vue.directive('input-digit', {
                         ? addDelimiter(convertedValue, block, delimiter)
                         : convertedValue
             }
-            if(binding.value.postfix) {
-                element.value = addPostfix(element.value, binding.value.postfix)
-            }
-            if(binding.value.prefix) {
-                element.value = addPrefix(element.value, binding.value.prefix)
-            }
         }
         element.dispatchEvent(event)
     }
@@ -81,11 +75,4 @@ function toEnNumber(number) {
         ''
     )
     return enStr
-}
-
-function addPrefix(str, prefix) {
-    return `${prefix} ${str}`
-}
-function addPostfix(str, postfix) {
-    return `${str} ${postfix}`
 }
